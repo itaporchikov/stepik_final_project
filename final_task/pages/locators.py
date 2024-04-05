2a00e03a48b23421a0c936166713b0f101be2846
i.taporchikov <ilya.taporchikov23@gmail.com>
2024-04-06 00:36:59 +0300

    Login page added

--- final_task/pages/locators.py
+++ final_task/pages/locators.py
@@ -1,4 +1,9 @@
 from selenium.webdriver.common.by import By
 
+
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+class LoginPageLocators():
+    LOGIN_FORM = (By.CSS_SELECTOR, "[name='login_submit']")
+    REGISTER_FORM = (By.CSS_SELECTOR, "[name='registration_submit']"
